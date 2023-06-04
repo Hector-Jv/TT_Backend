4b9c657e8ea29e04f8757a0b4c1e8c6ed882d804
Hector-Jv <hjaimev1300@alumno.ipn.mx>
2023-06-04 17:47:50 -0600

    m

--- app/routes/Sitios/mostrar_sitio.py
+++ app/routes/Sitios/mostrar_sitio.py
@@ -4,10 +4,9 @@ from app.classes.consulta import Consulta
 
 mostrar_sitio_bp = Blueprint('mostrar_sitio', __name__)
 
-@mostrar_sitio_bp.route('/mostrar_sitio/<int: cve_sitio>', methods=["GET", "POST"])
+@mostrar_sitio_bp.route('/mostrar_sitio/<int:cve_sitio>', methods=["GET", "POST"])
 def mostrar_info_sitio(cve_sitio):
     
-    data = request.get_json()
     
     conexion_bd = Consulta()
     datos_sitio = conexion_bd.obtener_sitio(cve_sitio)
