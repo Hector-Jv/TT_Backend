e6051a2887f05d4dbb380eeca7a4f4e1199b09b8
Hector-Jv <hjaimev1300@alumno.ipn.mx>
2023-06-25 02:54:53 -0600

    m

--- app/routes/Administrador/__init__.py
+++ app/routes/Administrador/__init__.py
@@ -4,6 +4,7 @@ from .inhabilitar_sitio import inhabilitar_sitio_bp
 from .modificar_sitio import modificar_sitio_bp
 from .mostrar_usuarios import mostrar_usuarios_bp
 from .mostrar_usuario import mostrar_usuario_bp
+from .inhabilitar_usuario import inhabilitar_usuario_bp
 
 
 all_blueprints = (
@@ -12,6 +13,7 @@ all_blueprints = (
     inhabilitar_sitio_bp,
     modificar_sitio_bp,
     mostrar_usuarios_bp,
-    mostrar_usuario_bp
+    mostrar_usuario_bp,
+    inhabilitar_usuario_bp
 )
 
